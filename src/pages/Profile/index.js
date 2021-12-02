1106b7b80db234fcb4486b842cfd873f68b1f77a
Aditya Malik <malikaditya2000@gmail.com>
2021-12-02 23:34:26 +0530

    bug

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -19,320 +19,184 @@ const { Meta } = Card;
 const { Option } = Select;
 
 const Profile = (props) => {
-  const [createBadgeModal, toggleCreateBadgeModal] = useState(false);
+    const [createBadgeModal, toggleCreateBadgeModal] = useState(false);
 
-  const handleMenteeChoose = (value) => {
-    console.log(value);
-  };
+    const handleMenteeChoose = (value) => {
+        console.log(value);
+    };
 
-  const onFinish = (values) => {
-    console.log("on finish called");
-    // const mentee = localStorage.getItem("docsrecordDoctor");
-    // values.doctor = doctor;
-    // axios
-    //   .put(`/patients/${id}`, values)
-    //   .then((response) => {
-    //     console.log(response);
-    //     setPatient(response.data);
-    //     message.success("Patient updated successfully !");
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  };
-  useEffect(() => {
-    const mentee = localStorage.getItem("mentee");
-    axios
-      .get(`/mentee/badges/${mentee}`)
-      .then((response) => {
-        console.log(response.data);
-        setBadges(response.data);
-      })
-      .catch((err) => {
-        if (!!err.response && err.response.status === 401) {
-          setTimeout(() => {
-            window.location.pathname = "/";
-          }, 1000);
-        }
-      });
+    const onFinish = (values) => {
+        console.log("on finish called");
+        // const mentee = localStorage.getItem("docsrecordDoctor");
+        // values.doctor = doctor;
+        // axios
+        //   .put(`/patients/${id}`, values)
+        //   .then((response) => {
+        //     console.log(response);
+        //     setPatient(response.data);
+        //     message.success("Patient updated successfully !");
+        //   })
+        //   .catch((err) => {
+        //     console.log(err);
+        //   });
+    };
+    useEffect(() => {
+        const mentee = localStorage.getItem("mentee");
+        axios
+            .get(`/mentee/badges/${mentee}`)
+            .then((response) => {
+                console.log(response.data);
+                setBadges(response.data);
+            })
+            .catch((err) => {
+                if (!!err.response && err.response.status === 401) {
+                    setTimeout(() => {
+                        window.location.pathname = "/";
+                    }, 1000);
+                }
+            });
 
-    axios
-      .get(`/mentee/${mentee}`)
-      .then((response) => {
-        console.log(response.data);
-        setmentee(response.data);
-        setMentors(response.data.mentors);
-      })
-      .catch((err) => {
-        if (!!err.response && err.response.status === 401) {
-          setTimeout(() => {
-            window.location.pathname = "/";
-          }, 1000);
-        }
-      });
+        axios
+            .get(`/mentee/${mentee}`)
+            .then((response) => {
+                console.log(response.data);
+                setmentee(response.data);
+                setMentors(response.data.mentors);
+            })
+            .catch((err) => {
+                if (!!err.response && err.response.status === 401) {
+                    setTimeout(() => {
+                        window.location.pathname = "/";
+                    }, 1000);
+                }
+            });
 
-    axios
-      .get(`/mentor/category`)
-      .then((response) => {
-        setCategories(response.data);
-        console.log(response.data);
-      })
-      .catch((err) => {
-        if (!!err.response && err.response.status === 401) {
-          setTimeout(() => {
-            window.location.pathname = "/";
-          }, 1000);
-        }
-      });
-  }, []);
-  const [badges, setBadges] = useState([]);
-  const [mentors, setMentors] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [categoryID, setCategoryID] = useState();
-  const [mentee, setmentee] = useState("");
-  const [selectedMentor, setSelectedMentor] = useState({});
-  const [mentorModal, toggleMentorModal] = useState(false);
-  let categoryList = categories.map((category) => {
-    return (
-      // onClick={() => handleCategoryFilter(category._id)}
-      <div>
-        <Card.Grid
-          style={{
-            width: "32%",
-            margin: "5px",
-            textAlign: "center",
-            color: "blue",
-            cursor: "pointer",
-          }}
-          cover={
-            <img
-              alt="example"
-              src={`data:image/${
-                category.image.contentType
-              };base64,${new Buffer.from(category.image.data).toString(
-                "base64"
-              )}`}
-            />
-          }
-        >
-          {category.name}
-        </Card.Grid>
-      </div>
-    );
-  });
-
-  let BadgesList = badges.map((badge) => {
-    return (
-      <div>
-        <Col>
-          <Card
-            hoverable
-            cover={
-              <img
-                alt="example"
-                src={`data:image/${
-                  badge.image.contentType
-                };base64,${new Buffer.from(badge.image.data).toString(
-                  "base64"
-                )}`}
-              />
-            }
-          >
-            <strong>Date : </strong> {badge.date}
-            <br />
-            <strong>Name : </strong> {badge.name}
-            <br />
-            <strong>Value : </strong> {badge.value}
-            <br />
-            <strong>Mentor Name : </strong>
-            {/* {badge.mentor.name} */}
-            <br />
-            <strong>Mentee Name : </strong> {badge.mentee.name}
-            <br />
-            <strong>Description : </strong> {badge.description}
-          </Card>
-        </Col>
-      </div>
-    );
-  });
-  let mentorList = mentors.map((mentor, index) => {
-    return (
-      <React.Fragment key={index}>
-        <Col md={12} sm={24} xs={24}>
-          <Card style={{ marginTop: 16 }}>
-            <Meta
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title={mentor.mentor.name}
-            />
-            <p>{mentor.mentor.email}</p>
-            <Row align="middle">
-              <Col span={12}>
-                <Button
-                  onClick={() => {
-                    setSelectedMentor(mentor);
-                    toggleMentorModal(true);
-                  }}
-                  size="small"
-                  style={{
-                    border: "none",
-                    color: "#1890FF",
-                    boxShadow: "none",
-                  }}
+        axios
+            .get(`/mentor/category`)
+            .then((response) => {
+                setCategories(response.data);
+                console.log(response.data);
+            })
+            .catch((err) => {
+                if (!!err.response && err.response.status === 401) {
+                    setTimeout(() => {
+                        window.location.pathname = "/";
+                    }, 1000);
+                }
+            });
+    }, []);
+    const [badges, setBadges] = useState([]);
+    const [mentors, setMentors] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [categoryID, setCategoryID] = useState();
+    const [mentee, setmentee] = useState("");
+    const [selectedMentor, setSelectedMentor] = useState({});
+    const [mentorModal, toggleMentorModal] = useState(false);
+    let categoryList = categories.map((category) => {
+        return (
+            // onClick={() => handleCategoryFilter(category._id)}
+            <div>
+                <Card.Grid
+                    style={{
+                        width: "32%",
+                        margin: "5px",
+                        textAlign: "center",
+                        color: "blue",
+                        cursor: "pointer",
+                    }}
+                    cover={
+                        <img
+                            alt="example"
+                            src={`data:image/${
+                                category.image.contentType
+                            };base64,${new Buffer.from(
+                                category.image.data
+                            ).toString("base64")}`}
+                        />
+                    }
                 >
-                  Click to View full details
-                </Button>
-              </Col>
-              <Col span={12}>
-                <a href={mentor.profileurl} target="_blank" rel="noreferrer">
-                  View LinkedIn Profile
-                </a>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-      </React.Fragment>
-    );
-  });
-
-  return (
-    <>
-      <Layout>
-        <SideMenu isMentor={props?.location?.pathname === "/mentorprofile"} />
-        <Layout style={{ backgroundColor: "white" }}>
-          <Content
-            style={{
-              height: "100vh",
-              marginLeft: "200px",
-              padding: "50px",
-            }}
-          >
-            <S.Heading>Profile</S.Heading>
-            <br />
-            <br />
-            <Row align="middle">
-              <Col span={12} style={{ textAlign: "center" }}>
-                <h3>Name :</h3>
-              </Col>
-              <Col span={12}>
-                <h3>{mentee.name}</h3>
-              </Col>
-            </Row>
-            <br />
-            <Row align="middle">
-              <Col span={12} style={{ textAlign: "center" }}>
-                <h3>Email :</h3>
-              </Col>
-              <Col span={12}>
-                <h3>{mentee.email}</h3>
-              </Col>
-            </Row>
-            <br />
-            <Row align="middle">
-              <Col span={12} style={{ textAlign: "center" }}>
-                <h3>LinkedIn Profile URL :</h3>
-              </Col>
-              <Col span={12}>
-                <h3>{mentee.profileurl}</h3>
-              </Col>
-            </Row>
-            <br />
-            <Row align="middle">
-              <Col span={12} style={{ textAlign: "center" }}>
-                <h1>YOUR COIN BALANCE</h1>
-              </Col>
-              <Col span={12}>
-                {mentee.totalCoins && <h1>{mentee.totalCoins.current}</h1>}
-              </Col>
-            </Row>
-            <br />
-            <Row align="middle">
-              <Col span={12} style={{ textAlign: "center" }}>
-                <h3>Achivements</h3>
-              </Col>
-              <Col span={12}>
-                <Input type="text" placeholder="Add achivements" />
-              </Col>
-            </Row>
-            <br />
-            <Row align="middle">
-              <Col span={12} style={{ textAlign: "center" }}>
-                <h3>Profile Heading</h3>
-              </Col>
-              <Col span={12}>
-                <Input type="text" placeholder="Profile Heading" />
-              </Col>
-            </Row>
-
-            <Modal
-              title="Mentor Details"
-              centered
-              visible={mentorModal}
-              footer={null}
-              onOk={() => toggleMentorModal(false)}
-              onCancel={() => toggleMentorModal(false)}
-            >
-              {selectedMentor.mentor && (
-                <div>
-                  <Row>
-                    <Col span={12}>
-                      <h4>Name : </h4>
-                    </Col>
-                    <Col span={12}>{selectedMentor.mentor.name}</Col>
-                  </Row>
-                  <Row>
-                    <Col span={12}>
-                      <h4>Email : </h4>
-                    </Col>
-                    <Col span={12}>{selectedMentor.mentor.email}</Col>
-                  </Row>
-                  <Row>
-                    <Col span={12}>
-                      <h4>Qualifications : </h4>
-                    </Col>
-                    <Col span={12}>{selectedMentor.mentor.qualifications}</Col>
-                  </Row>
-
-                  <Row>
-                    <Col span={24}>
-                      <a
-                        href={selectedMentor.profileurl}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        View LinkedIn Profile
-                      </a>
-                    </Col>
-                  </Row>
-                </div>
-              )}
-
-              <br />
-            </Modal>
-            <Row>
-              <Col span={24}>
-                <h2>
-                  My{" "}
-                  {props?.location?.pathname === "/menteeprofile"
-                    ? "Mentors"
-                    : "Mentees"}
-                </h2>
-                {mentorList}
-              </Col>
-            </Row>
-            <h2>My Badges</h2>
-            <Row align="middle">{BadgesList}</Row>
-            <br />
-            <div style={{ textAlign: "center" }}>
-              <Button
-                danger
-                onClick={() => {
-                  localStorage.clear();
-                  props.history.push("/");
-                }}
-              >
-                LOGOUT
-              </Button>
+                    {category.name}
+                </Card.Grid>
             </div>
+        );
+    });
+
+    let BadgesList = badges.map((badge) => {
+        return (
+            <div>
+                <Col>
+                    <Card
+                        hoverable
+                        cover={
+                            <img
+                                alt="example"
+                                src={`data:image/${
+                                    badge.image.contentType
+                                };base64,${new Buffer.from(
+                                    badge.image.data
+                                ).toString("base64")}`}
+                            />
+                        }
+                    >
+                        <strong>Date : </strong> {badge.date}
+                        <br />
+                        <strong>Name : </strong> {badge.name}
+                        <br />
+                        <strong>Value : </strong> {badge.value}
+                        <br />
+                        <strong>Mentor Name : </strong>
+                        {/* {badge.mentor.name} */}
+                        <br />
+                        <strong>Mentee Name : </strong> {badge.mentee.name}
+                        <br />
+                        <strong>Description : </strong> {badge.description}
+                    </Card>
+                </Col>
+            </div>
+        );
+    });
+    let mentorList = mentors.map((mentor, index) => {
+        return (
+            <React.Fragment key={index}>
+                <Col md={12} sm={24} xs={24}>
+                    <Card style={{ marginTop: 16 }}>
+                        <Meta
+                            avatar={
+                                <Avatar src="https://joeschmoe.io/api/v1/random" />
+                            }
+                            title={mentor.mentor.name}
+                        />
+                        <p>{mentor.mentor.email}</p>
+                        <Row align="middle">
+                            <Col span={12}>
+                                <Button
+                                    onClick={() => {
+                                        setSelectedMentor(mentor);
+                                        toggleMentorModal(true);
+                                    }}
+                                    size="small"
+                                    style={{
+                                        border: "none",
+                                        color: "#1890FF",
+                                        boxShadow: "none",
+                                    }}
+                                >
+                                    Click to View full details
+                                </Button>
+                            </Col>
+                            <Col span={12}>
+                                <a
+                                    href={mentor.profileurl}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    View LinkedIn Profile
+                                </a>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+            </React.Fragment>
         );
     });
 
